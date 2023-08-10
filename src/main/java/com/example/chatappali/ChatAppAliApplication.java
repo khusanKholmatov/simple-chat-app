df6f0d3b8eb5fb62502ca7860a4a3f0c760dbeae
khusanKholmatov <workacc1730@gmail.com>
2023-08-10 12:22:10 +0500

    one and only commit

---
+++ src/main/java/com/example/chatappali/ChatAppAliApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatappali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppAliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatAppAliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chatappali/ChatAppAliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatappali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppAliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
